94f0f9e78f983c1903a9dc898f6463ee0c4d3527
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2020-06-09 18:42:54 +0200

    ngrx: add action and reducer for flights loaded

--- apps/flight-app/src/app/flight-booking/+state/flight-booking.actions.ts
+++ apps/flight-app/src/app/flight-booking/+state/flight-booking.actions.ts
@@ -1,1 +1,7 @@
 import { createAction, props } from "@ngrx/store";
+import { Flight } from "@flight-workspace/flight-api";
+
+export const flightsLoaded = createAction(
+  "[FlightBooking] Flights Loaded",
+  props<{ flights: Flight[] }>()
+);
--- apps/flight-app/src/app/flight-booking/+state/flight-booking.reducer.ts
+++ apps/flight-app/src/app/flight-booking/+state/flight-booking.reducer.ts
@@ -1,6 +1,7 @@
 import { Action, createReducer, on } from "@ngrx/store";
 import * as FlightBookingActions from "./flight-booking.actions";
 import { Flight } from "@flight-workspace/flight-api";
+import { flightsLoaded } from "./flight-booking.actions";
 
 export const flightBookingFeatureKey = "flightBooking";
 
@@ -12,7 +13,10 @@ export const initialState: State = {
   flights: [],
 };
 
-const flightBookingReducer = createReducer(initialState);
+const flightBookingReducer = createReducer<State>(
+  initialState,
+  on(flightsLoaded, (state, action) => ({ ...state, flights: action.flights }))
+);
 
 export function reducer(state: State | undefined, action: Action) {
   return flightBookingReducer(state, action);
