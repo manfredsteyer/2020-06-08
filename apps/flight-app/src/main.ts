db20a3520652d11e4e3ba3f376d004d598eb661b
Manfred Steyer <manfred.steyer@gmx.net>
2020-06-10 14:28:52 +0200

    perf: onpush

--- apps/flight-app/src/main.ts
+++ apps/flight-app/src/main.ts
@@ -1,5 +1,7 @@
 import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {platformBrowser} from '@angular/platform-browser';
+
 
 import {AppModule} from './app/app.module';
 import {environment} from './environments/environment';
@@ -10,6 +12,11 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule);
 
+window['shell'].restartApp7 = function() {
+  
+  platformBrowserDynamic().bootstrapModule(AppModule);
+  
+}
 
 
 
