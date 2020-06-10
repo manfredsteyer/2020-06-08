41ca0646c13ba8f66dc4c8db986699494a4c2d1a
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2020-06-10 14:15:41 +0200

    [i18n] add translation in flight search

--- apps/flight-app/src/app/app.component.ts
+++ apps/flight-app/src/app/app.component.ts
@@ -1,12 +1,15 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'flight-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  constructor() { 
+export class AppComponent implements OnInit {
+  constructor(private translate: TranslateService) {}
+
+  ngOnInit(): void {
+    this.translate.use('de');
   }
 }
-
